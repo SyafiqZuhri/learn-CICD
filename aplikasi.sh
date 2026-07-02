1a100cb31cb1bfdc14ce9df54f61b72da482ab8e
SyafiqZuhri <syafiqmaulanazuhri@gmail.com>
2026-07-02 22:27:20 +0700

    mencoba mengubah nilai pajak

--- aplikasi.sh
+++ aplikasi.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "=== MENJALANKAN APLIKASI UTAMA ==="
-# Rumus Matematika: Total Belanja + Pajak
 TOTAL_BELANJA=50000
-PAJAK=5000
+PAJAK=10000
 TOTAL_BAYAR=$((TOTAL_BELANJA + PAJAK))
 
 echo "Total yang harus dibayar pelanggan: Rp $TOTAL_BAYAR"
