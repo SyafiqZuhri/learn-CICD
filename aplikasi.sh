2d878feb3c6b1adfc10cf733bccf08d24dbdb6ee
SyafiqZuhri <syafiqmaulanazuhri@gmail.com>
2026-07-03 15:56:20 +0700

    mencoba sistem otomatisasi CI/CD sampai ke server GCP

--- aplikasi.sh
+++ aplikasi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "=== MENJALANKAN APLIKASI UTAMA ==="
 TOTAL_BELANJA=50000
-PAJAK=10000
+PAJAK=5000
 TOTAL_BAYAR=$((TOTAL_BELANJA + PAJAK))
 
 echo "Total yang harus dibayar pelanggan: Rp $TOTAL_BAYAR"
